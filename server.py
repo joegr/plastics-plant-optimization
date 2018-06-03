9438ba327f8c658ac3a08b158c7550d88b18d900
Krishna Regmi <kisna72@gmail.com>
2018-06-03 18:14:28 -0500

    add waitress server

---
+++ server.py
@@ -0,0 +1,8 @@
+import os
+from waitress import serve 
+from whitenoise import WhiteNoise 
+from demo3d_django_project.wsgi import application
+
+if __name__ == '__main__':
+    static_root = os.path.join(os.path.realpath(__file__),'static')
+    serve(WhiteNoise(application, root=static_root, prefix='/static'),host='0.0.0.0', port=8001)
